ca6ce471232a321e8ffeeb98eb751e132532f344
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-19 20:12:03 -0300

    Criando tabela que renderizará a lista de categorias e adicionando botões de edita e excluir, e criando o botão de redirecionamento para criar nova categoria

--- resources/views/pages/categorias.blade.php
+++ resources/views/pages/categorias.blade.php
@@ -1,5 +1,48 @@
 @extends('pages.app',["current" => "register/public/categorias"])
 
 @section('body')
-<h1>Categorias</h1>
+<div class="card border">
+    <div class="card-body">
+        <h5 class="card-title">Cadastro de categorias</h5>
+
+        @if (count($categorias) > 0)
+                    
+        <table class="table table-ordered table-hover">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Nome da Categoria</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categorias as $cats)
+                    <tr>
+                        <td>{{$cats->id}}</td>
+                        <td>{{$cats->nome}}</td>
+                        <td>
+                            <a 
+                                 href="/register/public/categorias/editar/{{$cats->id}}"
+                                 class="btn btn-sm btn-primary"
+                            >
+                                Editar
+                            </a>
+                            <a 
+                                 href="/register/public/categorias/excluir/{{$cats->id}}"
+                                 class="btn btn-sm btn-danger"
+                            >
+                                Excluir
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
+        @endif
+    </div>
+    <div class="card-footer">
+        <a href="/register/public/categorias/novacategoria" class="btn btn-dark" role="button">Cadastrar Categoria</a>
+    </div>
+</div>
 @endsection
