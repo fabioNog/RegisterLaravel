0945cc16cfcf30bff638e963e632adc0cdd32a51
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-19 17:05:16 -0300

    Construi rotas para produtos e categorias

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/register/public/produtos', 'ProdutosController@index');
+Route::get('/register/public/categorias', 'CategoriasController@index');
