c56439728531b60b2dab32b7f4d78ad19b4d3946
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-19 19:27:20 -0300

    Adicionando meta csrf_token()

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token()}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Registro</title>
 
@@ -18,8 +19,7 @@
 </head>
 <body>
     <div class="container">
-        @component('navbar', ["current" => $current])
-            
+        @component('navbar', ["current" => $current])            
         @endcomponent
         <map role="main">
             {{-- se existir a sessão body então gera yield --}}
