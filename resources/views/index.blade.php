515d34cedf30044e63311f8115927e1afdd0c089
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-19 15:49:50 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('pages.app')
+
+@section('body')
+    <div class="jumbotron bg-light border border-secondary">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Cadastro de Produtos
+                        </h5>
+                        <p class="cart-text">Cadastre todos os seus produtos</p>
+                        <a href="/produtos" class="btn btn-primary">Cadastrar</a>
+                    </div>                                        
+                </div>
+                <div class="card border border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Cadastro de Produtos
+                        </h5>
+                        <p class="cart-text">Cadastre todos os seus produtos</p>
+                        <a href="/produtos" class="btn btn-primary">Cadastrar</a>
+                    </div>                                        
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
