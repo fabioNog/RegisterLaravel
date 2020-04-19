201f96425265f1371cd76fb9d4b9cdf2834d34e7
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-19 16:17:17 -0300

    Adicionando a navbar ao projeto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,21 +4,26 @@
     <div class="jumbotron bg-light border border-secondary">
         <div class="row">
             <div class="card-deck">
-                <div class="card border border-primary">
+                <div class="card border border-dark">
                     <div class="card-body">
                         <h5 class="card-title">
                             Cadastro de Produtos
                         </h5>
-                        <p class="cart-text">Cadastre todos os seus produtos</p>
+                        <p class="cart-text">
+                            Aqui você cadastra todos
+                            os seus produtos
+                        </p>
                         <a href="/produtos" class="btn btn-primary">Cadastrar</a>
                     </div>                                        
                 </div>
-                <div class="card border border-primary">
+                <div class="card border border-dark">
                     <div class="card-body">
                         <h5 class="card-title">
-                            Cadastro de Produtos
+                            Cadastro de Departamentos
                         </h5>
-                        <p class="cart-text">Cadastre todos os seus produtos</p>
+                        <p class="cart-text">
+                            Aqui você cadastra todos
+                            os seus departamentos</p>
                         <a href="/produtos" class="btn btn-primary">Cadastrar</a>
                     </div>                                        
                 </div>
--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -6,9 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Registro</title>
+
+    <style>
+        body{
+            padding: 20px;
+        }
+        .navbar{
+            margin-bottom: 20px; 
+        }
+    </style>
 </head>
 <body>
     <div class="container">
+        @component('navbar')
+            
+        @endcomponent
         <map role="main">
             {{-- se existir a sessão body então gera yield --}}
             @hasSection ('body')
