2cfc75a91d2964fc07f6003d22d09e70acfb3651
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-21 11:28:33 -0300

    alterando nome de rota

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
 });
 
 // Rotas Produtos
-Route::get('/register/public/produtos/produtos', 'ProdutosController@index');
+Route::get('/register/public/produtos', 'ProdutosController@index');
 Route::get('/register/public/produtos/novoproduto', 'ProdutosController@create');
-Route::post('/register/public/produtos/produtos', 'ProdutosController@store');
+Route::post('/register/public/produtos', 'ProdutosController@store');
 Route::get('/register/public/produtos/excluir/{id}', 'ProdutosController@destroy');
 Route::get('/register/public/produtos/editar/{id}', 'ProdutosController@edit');
 Route::post('/register/public/produtos/{id}', 'ProdutosController@update');
